00d87df4d07641a719201e36b5ff29a79f70ec0d
Timothy Jensen <tim@timjensen.us>
2017-06-06 14:05:45 -0400

    Updated get_all_custom_field_meta() to latest version.

--- src/functions/helper-functions.php
+++ src/functions/helper-functions.php
@@ -12,22 +12,27 @@
 namespace ForwardJump\Utility\Functions;
 
 /**
- * Retrieves all post meta data according to the structure in the $config array.
+ * Retrieves all post meta data according to the structure in the $config
+ * array.
  *
- * Provides a convenient and more performant alternative to ACF's `get_field()`.
+ * Provides a convenient and more performant alternative to ACF's
+ * `get_field()`.
  *
- * This function is especially useful when working with ACF repeater fields and flexible content layouts.
+ * This function is especially useful when working with ACF repeater fields and
+ * flexible content layouts.
  *
- * @version 1.1.1
+ * @link    https://www.timjensen.us/acf-get-field-alternative/
+ *
+ * @version 1.2.1
  *
  * @param integer $post_id Required. Post ID.
- * @param array   $config  Required. An array that represents the structure of the custom fields.
- *                         Follows the same format as the ACF export field groups array.
- * @param string  $prefix  The necessary meta_key prefix is generated automatically as the function iterates.
+ * @param array   $config  Required. An array that represents the structure of
+ *                         the custom fields. Follows the same format as the
+ *                         ACF export field groups array.
  *
  * @return array
  */
-function get_all_custom_field_meta( $post_id, $config, $prefix = '' ) {
+function get_all_custom_field_meta( $post_id, array $config ) {
 
 	$results = array();
 
@@ -39,46 +44,48 @@ function get_all_custom_field_meta( $post_id, $config, $prefix = '' ) {
 
 		$meta_key = $field['name'];
 
-		if ( $prefix ) {
-			$meta_key = $prefix . $meta_key;
+		if ( isset( $field['meta_key_prefix'] ) ) {
+			$meta_key = $field['meta_key_prefix'] . $meta_key;
 		}
 
 		$field_value = get_post_meta( $post_id, $meta_key, true );
 
 		if ( isset( $field['layouts'] ) ) { // We're dealing with flexible content layouts.
 
-			// Get array position of the current layout type.
-			$layout_keys = array_flip( wp_list_pluck( $field['layouts'], 'name' ) );
+			// Build a keyed array of possible layout types.
+			$layout_types = [];
+			foreach ( $field['layouts'] as $key => $layout_type ) {
+				$layout_types[ $layout_type['name'] ] = $layout_type;
+			}
 
-			foreach ( $field_value as $key => $layout_row ) {
-				$prefix     = $meta_key . "_{$key}_";
-				$new_config = $field['layouts'][ $layout_keys[ $layout_row ] ]['sub_fields'];
+			foreach ( $field_value as $key => $current_layout_type ) {
+				$new_config = $layout_types[ $current_layout_type ]['sub_fields'];
+
+				foreach ( $new_config as &$field_config ) {
+					$field_config['meta_key_prefix'] = $meta_key . "_{$key}_";
+				}
 
 				$results[] = array_merge(
 					[
-						'acf_fc_layout' => $layout_row,
+						'acf_fc_layout' => $current_layout_type,
 					],
-					get_all_custom_field_meta( $post_id, $new_config, $prefix )
+					get_all_custom_field_meta( $post_id, $new_config )
 				);
 			}
-
-			// reset.
-			$prefix = '';
-
 		} elseif ( isset( $field['sub_fields'] ) ) { // We're dealing with repeater fields.
 
 			for ( $i = 0; $i < $field_value; $i ++ ) {
-				$prefix                      = $meta_key . "_{$i}_";
-				$new_config                  = $field['sub_fields'];
-				$results[ $field['name'] ][] = get_all_custom_field_meta( $post_id, $new_config, $prefix );
+				$new_config = $field['sub_fields'];
+
+				foreach ( $new_config as &$field_config ) {
+					$field_config['meta_key_prefix'] = $meta_key . "_{$i}_";
+				}
+
+				$results[ $field['name'] ][] = get_all_custom_field_meta( $post_id, $new_config );
 			}
-
-			// reset.
-			$prefix = '';
-
 		} else {
 			$results[ $field['name'] ] = $field_value;
-		}
+		} // End if().
 	} // End foreach().
 
 	return $results;
