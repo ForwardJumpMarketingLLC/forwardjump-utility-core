bbc17d6cfe166c3eae9ff65a753cab7726dcb5d2
Timothy Jensen <tim@timjensen.us>
2017-06-19 05:26:26 -0400

    Required CMB2/init.php in tests bootstrap to fix failing tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,8 +17,9 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-//	require dirname( dirname( __FILE__ ) ) . '/vendor/autoload.php';
 	require dirname( dirname( __FILE__ ) ) . '/bootstrap.php';
+	require_once dirname( dirname( __FILE__ ) ) . '/vendor/CMB2/init.php';
+
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
