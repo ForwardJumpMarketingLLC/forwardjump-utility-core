7198d0d0b17a0df1c5c89f91ee9c8c301aa0d3c6
Walter Lopez <walter@wpallied.com>
2019-06-24 16:19:18 -0600

    Update plugin version

--- bootstrap.php
+++ bootstrap.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/ForwardJumpMarketingLLC/forwardjump-utility-core
  * Description: The ForwardJump core functionality plugin.
  *
- * Version: 1.6.1
+ * Version: 1.6.3
  *
  * Author: Tim Jensen
  * Author URI: https://forwardjump.com/
